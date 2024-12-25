353b75ea608ccdec64c8e5a9b734585427b7e991
jongbinchoi <jongbin985@naver.com>
2024-12-25 14:14:20 +0900

    study : 싸이월드 3탄

--- class/03-javascript/06-timer.js
+++ class/03-javascript/06-timer.js
@@ -7,7 +7,7 @@ let number = () => {
 
 	setInterval(function () {
 		if (time >= 0) {
-			let min = Math.floor(time / 60); // 분 계산
+			let min = Math.floor(time / 60); // 분 계산, floor 소수점 날려버림
 			let sec = String(time % 60).padStart(2, "0"); // 초 계산, 두 자리로 출력
 			document.getElementById("timer").innerText = `${min}:${sec}`; // 타이머 값 갱신
 			time -= 1; // 시간을 1초씩 감소
